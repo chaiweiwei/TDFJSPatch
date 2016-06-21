79a250ccd3c283100e5ca79594ae297c2e00cd0a
chaiweiwei <1119191759@qq.com>
2016-06-21 15:29:46 +0800

    Initial Commit

---
+++ TDFJSPatch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TDFJSPatch
+//
+//  Created by chaiweiwei on 16/6/21.
+//  Copyright © 2016年 chaiweiwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
